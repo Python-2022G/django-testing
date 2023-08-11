099910d5076d72aa0af6f92a3d8af1c1dd39a40f
djumanov <diyorbekjumanov1120@gmail.com>
2023-08-11 16:21:42 +0500

    model test

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Product
+
+
+class ProductTestCase(TestCase):
+    def test_create_model(self):
+        product = Product.objects.create(
+            name='p01',
+            description='d01',
+            price=9.7
+        )
+
+        self.assertEqual(Product.objects.count(), 1)
+        self.assertEqual(Product.objects.first().name, 'p01')
+
