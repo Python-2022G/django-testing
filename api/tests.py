e610bf676851672413df56fd342610a8e2a47177
djumanov <diyorbekjumanov1120@gmail.com>
2023-08-12 09:55:02 +0500

    test view

--- api/tests.py
+++ api/tests.py
@@ -19,3 +19,32 @@ class ProductTestCase(TestCase):
 
         self.assertEqual(Product.objects.count(), 1)
 
+class ProductListViewTestCase(TestCase):
+    def test_getting_products(self):
+        Product.objects.create(
+            name='p01',
+            description='d01',
+            price=9.7
+        )
+
+        response = self.client.get('/api/products/')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), [{"id": 1, "name": "p01", "description": "d01", "price": 9.7}])
+
+    def test_create_product(self):
+        data = {
+            "name": "p02",
+            "description": "d02",
+            "price": 8.3
+        }
+
+        response = self.client.post('/api/products/', data=data, content_type='application/json')
+
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.json(), {
+            "id": 1,
+            "name": "p02",
+            "description": "d02",
+            "price": 8.3
+        })
--- api/views.py
+++ api/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.http import HttpRequest, JsonResponse
+from django.views import View
+from django.forms.models import model_to_dict
+from .models import Product
+import json
 
-# Create your views here.
+
+class ProductListView(View):
+    def get(self, request: HttpRequest) -> JsonResponse:
+        products = Product.objects.all()
+
+        data = [model_to_dict(product) for product in products]
+
+        return JsonResponse(data, safe=False)
+
+
+    def post(self, reqeust: HttpRequest) -> JsonResponse:
+        data = json.loads(reqeust.body.decode('utf-8'))
+
+        product = Product.objects.create(
+            name=data['name'],  
+            description=data['description'],
+            price=data['price']
+        )
+
+        product.save()
+
+        return JsonResponse(model_to_dict(product), status=201)
+
+    
